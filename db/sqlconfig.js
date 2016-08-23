37aff2241664184b7cff70c8d48cc6e8978beeaf
Christine Chou <christinejchou@gmail.com>
2016-08-23 11:34:55 -0700

    Update sql password

--- db/sqlconfig.js
+++ db/sqlconfig.js
@@ -8,7 +8,7 @@ const Sequelize = require('sequelize');
 //     min: 0
 //   },
 // });
-const sequelize = new Sequelize('reddi2mingle', 'root', '', {
+const sequelize = new Sequelize('reddi2mingle', 'root', 'root', {
   dialect: 'mysql',
 });
 
